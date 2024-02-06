143939aa0d87c3062b938b6b16dda163820e8ca3
TeomanDeniz <te-odeniz@hotmail.com>
2024-02-06 18:13:08 +0300

    2024/02/06
    Updated sytanx to COBOL 1960, Added IO.cbl

--- IF-ELSE.cbl
+++ IF-ELSE.cbl
@@ -1,32 +1,45 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. EXAMPLE-IF-ELSE.
-DATA DIVISION.
-	WORKING-STORAGE SECTION.
-		77 EAX PIC 99.
-
-PROCEDURE DIVISION.
-0001-MAIN.
-	MOVE 10 TO EAX.
-
-	IF EAX = 10
-		DISPLAY 'EAX IS 10.'
-	END-IF.
-
-	IF EAX <> 10
-		DISPLAY 'EAX IS NOT 10.'
-	END-IF.
-
-	IF EAX > 5
-		DISPLAY 'EAX IS GREATER THAN 5.'
-	ELSE
-		DISPLAY 'EAX IS NOT GREATER THAN 5.'
-	END-IF.
-
-STOP RUN.
-
-*> =  - EQUAL
-*> <> - NOT EQUAL
-*> <  - LESS
-*> >  - GREAHER
-*> => - EQUAL OR GREATHER
-*> =< - EQUAL OR LESS
+000000 *> ................................................................... <*
+000000 *> CBL - IF-ELSE                          :      Maximum Tension       <*
+000000 *> .......................................:........................... <*
+000000 *>                                        :     -__            __-     <*
+000000 *> Teoman Deniz                           : :    :!1!-_    _-!1!:    : <*
+000000 *> maximum-tension.com                    : ::                      :: <*
+000000 *>                                        : :!:    : :: : :  :  ::::!: <*
+000000 *> ...................................... :  :!:: :!:!1:!:!::1:::!!!:  <*
+000000 *> : C - Maximum Tension : C 2024/02/03 : :  ::!::!!1001010!:!11!!::   <*
+000000 *> :.....................:..............: :  :!1!!11000000000011!!:    <*
+000000 *> : License - NON       : U 2024/02/06 : :   ::::!!!1!!1!!!1!!!::     <*
+000000 *> :.....................:..............: :      ::::!::!:::!::::      <*
+000000 *> .......................................:........................... <*
+000000 *> IF VAR =  - EQUAL
+000000 *> IF VAR <> - NOT EQUAL
+000000 *> IF VAR <  - LESS
+000000 *> IF VAR >  - GREAHER
+000000 *> IF VAR => - EQUAL OR GREATHER
+000000 *> IF VAR =< - EQUAL OR LESS
+000100	IDENTIFICATION DIVISION.
+000200	PROGRAM-ID. EXAMPLE-IF-ELSE.
+000300	DATA DIVISION.
+000400		WORKING-STORAGE SECTION.
+000500			77 VAR_NUMBER PIC 99.
+000600	PROCEDURE DIVISION.
+000700	0001-MAIN.
+000800		MOVE 10 TO VAR_NUMBER.
+000900		*> ........................ [v] EQUAL [v] ....................... <*
+001000		IF VAR_NUMBER = 10
+001100			DISPLAY 'VAR_NUMBER IS 10.'
+001200		END-IF.
+001300		*> ........................ [^] EQUAL [^] ....................... <*
+001400		*> ..................... [v] NOT EQUAL [v] ...................... <*
+001500		IF VAR_NUMBER <> 10
+001600			DISPLAY 'VAR_NUMBER IS NOT 10.'
+001700		END-IF.
+001800		*> ..................... [^] NOT EQUAL [^] ...................... <*
+001900		*> ........................ [v] ELSE [v] ........................ <*
+002000		IF VAR_NUMBER > 5
+002100			DISPLAY 'VAR_NUMBER IS GREATER THAN 5.'
+002200		ELSE
+002300			DISPLAY 'VAR_NUMBER IS NOT GREATER THAN 5.'
+002400		END-IF.
+002500		*> ........................ [^] ELSE [^] ........................ <*
+002600		STOP RUN.
--- PARAGRAPHS.cbl
+++ PARAGRAPHS.cbl
@@ -1,19 +1,29 @@
-*> GO TO <- GO TO A PARAGRAPH
-*> PERFORM <- CALL A PARAGRAPH
-*> EXIT <- EXIT FROM A PARAGRAPH THAT CALLED VIA "PERFROM"
-IDENTIFICATION DIVISION.
-PROGRAM-ID. EXAMPLE-PARAGRAPHS.
-
-PROCEDURE DIVISION.
-0001-MAIN.
-	PERFORM TEST-PARAGRAPH. *> CALL PARAGRAPH
-	DISPLAY 'CONTINUING PROCESS...'.
-	GO TO EXIT-PROGRAM. *> GO TO A PARAGRAPH
-
-TEST-PARAGRAPH. *> (X EAX) <*
-	DISPLAY 'HERE WE ARE IN THE PARAGRAPH'.
-	EXIT. *> EXIT PARAGRAPH AND CONTINUE TO PARENT PARAGRAPH
-
-EXIT-PROGRAM.
-	DISPLAY 'PROGRAM CLOSING...'.
-	STOP RUN.
+000000 *> ................................................................... <*
+000000 *> CBL - PARAGRAPHS                       :      Maximum Tension       <*
+000000 *> .......................................:........................... <*
+000000 *>                                        :     -__            __-     <*
+000000 *> Teoman Deniz                           : :    :!1!-_    _-!1!:    : <*
+000000 *> maximum-tension.com                    : ::                      :: <*
+000000 *>                                        : :!:    : :: : :  :  ::::!: <*
+000000 *> ...................................... :  :!:: :!:!1:!:!::1:::!!!:  <*
+000000 *> : C - Maximum Tension : C 2024/02/03 : :  ::!::!!1001010!:!11!!::   <*
+000000 *> :.....................:..............: :  :!1!!11000000000011!!:    <*
+000000 *> : License - NON       : U 2024/02/06 : :   ::::!!!1!!1!!!1!!!::     <*
+000000 *> :.....................:..............: :      ::::!::!:::!::::      <*
+000000 *> .......................................:........................... <*
+000000 *> GO TO <PARAGPRAH>. <- GO TO A PARAGRAPH
+000000 *> PERFORM <PARAGRAPH>. <- CALL A PARAGRAPH
+000000 *> EXIT. <- EXIT FROM A PARAGRAPH THAT CALLED VIA "PERFROM"
+000100	IDENTIFICATION DIVISION.
+000200	PROGRAM-ID. EXAMPLE-PARAGRAPHS.
+000300	PROCEDURE DIVISION.
+000400	0001-MAIN.
+000500		PERFORM TEST-PARAGRAPH.
+000600		DISPLAY 'CONTINUING PROCESS...'.
+000700		GO TO EXIT-PROGRAM.
+000800	TEST-PARAGRAPH.
+000900		DISPLAY 'HERE WE ARE IN THE PARAGRAPH'.
+001000	EXIT.
+001100	EXIT-PROGRAM.
+001200		DISPLAY 'PROGRAM CLOSING...'.
+001300		STOP RUN.
